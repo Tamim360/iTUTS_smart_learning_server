48cd025391f66497619fcb3685d372b5a57b7425
Tamimul Islam <hmtamim24@gmail.com>
2022-10-26 22:10:02 +0600

    category wise description added

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
+app.get("/courses/all", (req, res) => {
+  res.send(courses)
+})
+
 app.get("/courses/:category", (req, res) => {
   const category = req.params.category;
   const selected = courses.filter(
@@ -22,6 +26,8 @@ app.get("/courses/:category", (req, res) => {
   );
   res.send(selected);
 });
+
+
 
 app.listen(port, () => {
   console.log("server listening on port", port);
